b82965a36ccd6afbc1d8d1989cbb4d457a6a09e7
Nagkumar <me@nagkumar.com>
2015-11-26 20:52:33 -0800

    Created a user model and overrode django admin user

--- splurge/core/models.py
+++ splurge/core/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class AppUser(AbstractUser):
+    REQUIRED_FIELDS = ['email']
+
+    organisation_name = models.CharField(max_length=255, null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'App User'
+        verbose_name_plural = 'App Users'
